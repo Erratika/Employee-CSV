60ff5ace506d2cef0071458a3574232328b921c0
Marc Murray <mmarc.96@gmail.com>
2022-07-12 22:41:39 +0100

    Adjust tests and add exceptions for null or empty entries. Change mNameInitial and gender to use char instead.

--- src/test/java/com/sparta/AlphaTeam/DataManagement/EmployeeConverterTest.java
+++ src/test/java/com/sparta/AlphaTeam/DataManagement/EmployeeConverterTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class EmployeeConverterTest {
 	/**
 	 * Test cases.
-	 * Valid Varying data.
+	 * Valid data.
 	 * Missing or empty data ie no data between commas.
 	 * Invalid dates.
 	 * Invalid emails.
@@ -28,56 +28,32 @@ class EmployeeConverterTest {
 			assertEquals(792050,actualEmployee.getId());
 			assertEquals("Ms.",actualEmployee.getPrefix());
 			assertEquals("Marget",actualEmployee.getfName());
-			assertEquals("V",actualEmployee.getmName());
+			assertEquals('V',actualEmployee.getmName());
 			assertEquals("Bowden",actualEmployee.getlName());
-			assertEquals("F",actualEmployee.getGender());
+			assertEquals('F',actualEmployee.getGender());
 			assertEquals("marget.bowden@hotmail.com",actualEmployee.getEmail());
-			SimpleDateFormat parser = new SimpleDateFormat("dd/MM/yyyy");
-			assertEquals(parser.parse("10/23/1991"),actualEmployee.getDateOfBirth());
+			SimpleDateFormat parser = new SimpleDateFormat("MM/dd/yyyy");
+			assertEquals(parser.parse("23/10/1991"),actualEmployee.getDateOfBirth());
 			assertEquals(parser.parse("7/8/2015"),actualEmployee.getJoinDate());
 			assertEquals(147396,actualEmployee.getSalary());
 		});
 
 	}
 	@Test
-	void convertStringsToEmployeesTestInvalidDate() {
+	void convertStringsToEmployeesTestEmptyDate() {
 		List<String> testValidStrings = new ArrayList<>();
-		testValidStrings.add("792050,Ms.,Marget,V,Bowden,F,marget.bowden@hotmail.com,10/23/1991,7/8/2015,147396\n");
-		// 10/11/1992
+		testValidStrings.add("792050,Ms.,Marget,V,Bowden,F,marget.bowden@hotmail.com,,7/8/2015,147396\n");
 		assertThrows(ParseException.class, ()->{
-			List<Employee> actualEmployees = EmployeeConverter.convertStringsToEmployees(testValidStrings);
-			Employee actualEmployee = actualEmployees.get(0);
-			assertEquals(792050,actualEmployee.getId());
-			assertEquals("Ms.",actualEmployee.getPrefix());
-			assertEquals("Marget",actualEmployee.getfName());
-			assertEquals("V",actualEmployee.getmName());
-			assertEquals("Bowden",actualEmployee.getlName());
-			assertEquals("F",actualEmployee.getGender());
-			assertEquals("marget.bowden@hotmail.com",actualEmployee.getEmail());
-			SimpleDateFormat parser = new SimpleDateFormat("MM/dd/yyyy");
-			assertEquals(parser.parse("10/23/1991"),actualEmployee.getDateOfBirth());
-			assertEquals(parser.parse("7/8/2015"),actualEmployee.getJoinDate());
-			assertEquals(147396,actualEmployee.getSalary());
+			EmployeeConverter.convertStringsToEmployees(testValidStrings);
+
 		});
 	}
 	@Test
-	void convertStringsToEmployeesTestEmpty() {
+	void convertStringsToEmployeesTestEmptyChar() {
 		List<String> testValidStrings = new ArrayList<>();
-		testValidStrings.add(",Ms.,Marget,V,Bowden,,marget.bowden@hotmail.com,,7/8/2015,147396\n");
-		assertThrows(ParseException.class, ()->{
-			List<Employee> actualEmployees = EmployeeConverter.convertStringsToEmployees(testValidStrings);
-			Employee actualEmployee = actualEmployees.get(0);
-			assertEquals(0,actualEmployee.getId());
-			assertEquals("Ms.",actualEmployee.getPrefix());
-			assertEquals("Marget",actualEmployee.getfName());
-			assertEquals("V",actualEmployee.getmName());
-			assertEquals("Bowden",actualEmployee.getlName());
-			assertEquals("F",actualEmployee.getGender());
-			assertEquals("marget.bowden@hotmail.com",actualEmployee.getEmail());
-			SimpleDateFormat parser = new SimpleDateFormat("MM/dd/yyyy");
-			assertEquals(null,actualEmployee.getDateOfBirth());
-			assertEquals(parser.parse("7/8/2015"),actualEmployee.getJoinDate());
-			assertEquals(147396,actualEmployee.getSalary());
+		testValidStrings.add(",Ms.,Marget,V,Bowden,,marget.bowden@hotmail.com,23/10/1991,7/8/2015,147396\n");
+		assertThrows(StringIndexOutOfBoundsException.class, ()->{
+			EmployeeConverter.convertStringsToEmployees(testValidStrings);
 		});
 	}
 
