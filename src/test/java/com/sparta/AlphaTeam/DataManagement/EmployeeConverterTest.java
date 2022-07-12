9ed57947cfbb40aee1fe41246bb3197b6390f283
Marc Murray <mmarc.96@gmail.com>
2022-07-12 16:13:27 +0100

    Move CSV files and add .idea to gitignore

--- src/test/java/com/sparta/AlphaTeam/DataManagement/EmployeeConverterTest.java
+++ src/test/java/com/sparta/AlphaTeam/DataManagement/EmployeeConverterTest.java
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 package com.sparta.AlphaTeam.DataManagement;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class EmployeeConverterTest {
-
-=======
 import static org.junit.jupiter.api.Assertions.*;
 class EmployeeConverterTest {
   
->>>>>>> 5782381 (Added parser - requires testing)
 }
