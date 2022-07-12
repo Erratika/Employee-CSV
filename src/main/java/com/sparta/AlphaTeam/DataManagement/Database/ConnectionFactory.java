cdaa1cc461ef4f14e523a384ef087145ab8e56c1
Jeffrey champion <Jeffrey.champion42@gmail.com>
2022-07-12 16:59:04 +0100

    possible table creation made

--- src/main/java/com/sparta/AlphaTeam/DataManagement/Database/ConnectionFactory.java
+++ src/main/java/com/sparta/AlphaTeam/DataManagement/Database/ConnectionFactory.java
@@ -13,7 +13,7 @@ public class ConnectionFactory implements AutoCloseable {
     public static Connection getConnection(){
         Properties dbProps = new Properties();
         try {
-            dbProps.load(new FileReader("src/main/java/com/sparta/sakila/database.properties"));
+            dbProps.load(new FileReader("src/main/java/com/sparta/AlphaTeam/DataManagement/Database/database.properties"));
         } catch (IOException e) {
             e.printStackTrace();
 
--- src/main/java/com/sparta/AlphaTeam/DataManagement/Database/DatabaseInit.java
+++ src/main/java/com/sparta/AlphaTeam/DataManagement/Database/DatabaseInit.java
@@ -1,5 +1,40 @@
 package com.sparta.AlphaTeam.DataManagement.Database;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DatabaseInit {
+
+    public static void makeTable(){
+        Connection conn = ConnectionFactory.getConnection();
+        try(Statement statement = conn.createStatement()){
+            statement.executeUpdate("DROP TABLE IF EXISTS employees");
+
+            String sql = "CREATE TABLE `employees`" +
+                    " (`emplyee_id` int NOT NULL," +
+                    "`prefix` varchar(10) DEFAULT NULL," +
+                    "`first_name` varchar(45) DEFAULT NULL," +
+                    "`middle_initial` char(1) DEFAULT NULL," +
+                    "`last_name` varchar(45) DEFAULT NULL," +
+                    "`gender` char(1) DEFAULT NULL," +
+                    "`email` varchar(45) DEFAULT NULL," +
+                    "`date_of_birth` date DEFAULT NULL," +
+                    "`date_of_joining` date DEFAULT NULL," +
+                    "`salary` int DEFAULT NULL," +
+                    " PRIMARY KEY (`emplyee_id`)," +
+                    " UNIQUE KEY `email_UNIQUE` (`email`))" +
+                    " ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 " +
+                    "COLLATE=utf8mb4_0900_ai_ci";
+        statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+        ConnectionFactory.closeConnection();
+    }
 
 }
