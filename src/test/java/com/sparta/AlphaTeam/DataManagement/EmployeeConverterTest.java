f59aff8c637cbc11713619a6cfaf1692150f1e9e
Marc Murray <mmarc.96@gmail.com>
2022-07-12 16:02:34 +0100

    Change employee to public

---
+++ src/test/java/com/sparta/AlphaTeam/DataManagement/EmployeeConverterTest.java
@@ -0,0 +1,13 @@
+<<<<<<< HEAD
+package com.sparta.AlphaTeam.DataManagement;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeConverterTest {
+
+=======
+import static org.junit.jupiter.api.Assertions.*;
+class EmployeeConverterTest {
+  
+>>>>>>> 5782381 (Added parser - requires testing)
+}
