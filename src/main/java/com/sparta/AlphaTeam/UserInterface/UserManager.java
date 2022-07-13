0a0224047f653af4a70d042a5a203951dda26fb4
kira-coke <kiracoke@outlook.com>
2022-07-13 14:24:12 +0100

    Update UserManager.java

--- src/main/java/com/sparta/AlphaTeam/UserInterface/UserManager.java
+++ src/main/java/com/sparta/AlphaTeam/UserInterface/UserManager.java
@@ -2,7 +2,9 @@ package com.sparta.AlphaTeam.UserInterface;
 
 import com.sparta.AlphaTeam.DataManagement.Employee;
 
+import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Scanner;
 
 public class UserManager {
     public void displayRecords(List<Employee> listToRead){
@@ -10,5 +12,29 @@ public class UserManager {
             System.out.println(e);
         }
     }
+    public void promt(String string){
+        System.out.println(string);
+    }
+
+    public void promt(int x){
+        System.out.println(x);
+    }
+    public int getInputInt() throws ArithmeticException{
+        Scanner sc = new Scanner(System.in);
+        return sc.nextInt();
+    }
+    public String getInputString() throws InputMismatchException {
+        Scanner sc = new Scanner(System.in);
+        String temp = null;
+        try{
+            temp = sc.nextLine();
+        }catch(InputMismatchException e){
+            e.printStackTrace();
+        }
+        return temp;//users decision
+    }
+
+
+
 
 }
