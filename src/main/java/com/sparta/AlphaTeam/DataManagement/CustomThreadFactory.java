647960f64cbb368ae6dba9995c21e2adbbd88d1d
M-Matson <108122866+M-Matson@users.noreply.github.com>
2022-07-13 17:04:59 +0100

    Custom Thread Factory update

--- src/main/java/com/sparta/AlphaTeam/DataManagement/CustomThreadFactory.java
+++ src/main/java/com/sparta/AlphaTeam/DataManagement/CustomThreadFactory.java
@@ -1,18 +1,25 @@
 package com.sparta.AlphaTeam.DataManagement;
 
+import java.util.List;
 import java.util.concurrent.ThreadFactory;
 
 public class CustomThreadFactory implements ThreadFactory {
-    int threadCount;
-
-    public int getThreadCount() {
-        return threadCount;
-    }
+//    int threadCount;
+//
+//    public int getThreadCount() {
+//        return threadCount;
+//    }
 
     @Override
     public Thread newThread(Runnable r) {
 
-        threadCount++;
+//        threadCount++;
         return new Thread(r);
+    }
+    public void customThreadFactory(int threadCount, List<Employee> employeeList){
+        Thread[] threadArray = new Thread[threadCount];
+        Employee[][] powerRangers = new Employee[threadCount][];
+
+
     }
 }
