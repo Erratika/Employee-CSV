070c1f5fee0c97fda0fa9dfadab958bf03dcf866
kira-coke <kiracoke@outlook.com>
2022-07-13 15:19:27 +0100

    Update UserManager.java

--- src/main/java/com/sparta/AlphaTeam/UserInterface/UserManager.java
+++ src/main/java/com/sparta/AlphaTeam/UserInterface/UserManager.java
@@ -3,7 +3,10 @@ package com.sparta.AlphaTeam.UserInterface;
 import com.sparta.AlphaTeam.DataManagement.Employee;
 import com.sparta.AlphaTeam.UserInterface.Reader.Reader;
 
+import java.io.File;
+import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Scanner;
 
 public class UserManager {
     public void displayRecords(List<Employee> listToRead){
@@ -15,4 +18,43 @@ public class UserManager {
         return Reader.readFile(filePath);
     }
 
+    public void promt(String string){
+        System.out.println(string);
+    }
+    public void promt(int x){
+        System.out.println(x);
+    }
+    public int getInputInt() throws ArithmeticException{
+        Scanner sc = new Scanner(System.in);
+        return sc.nextInt();
+    }
+    public String getInputString() throws InputMismatchException {
+        Scanner sc = new Scanner(System.in);
+        String temp = null;
+        try{
+            temp = sc.nextLine();
+        }catch(InputMismatchException e){
+            e.printStackTrace();
+        }
+        return temp;//users decision
+    }
+    public FileEnum getFileNumber(){
+        Scanner sc = new Scanner(System.in);
+        FileEnum file = null;
+        boolean userChoosing = true;
+        while(userChoosing){
+            int fileNumber = sc.nextInt();
+            for(FileEnum s: FileEnum.values()){
+                if(s.getSortKey() == fileNumber){
+                    userChoosing = false;
+                    file = s;
+                }
+            }
+            if (userChoosing){
+                promt("Please enter a valid number corresponding to a File path");
+            }
+        }
+        return file;
+    }
 }
+
