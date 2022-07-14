19b101c586f1b643585702db86f5f28a3d32aa9c
Marc Murray <mmarc.96@gmail.com>
2022-07-14 18:02:07 +0100

    Fix EmployeeConerterTest

--- src/test/java/com/sparta/AlphaTeam/DataManagement/EmployeeConverterTest.java
+++ src/test/java/com/sparta/AlphaTeam/DataManagement/EmployeeConverterTest.java
@@ -53,9 +53,12 @@ class EmployeeConverterTest {
 	@Test
 	void convertStringsToEmployeesTestEmptyChar() {
 		List<String> testValidStrings = new ArrayList<>();
-		testValidStrings.add(",Ms.,Marget,V,Bowden,,marget.bowden@hotmail.com,23/10/1991,7/8/2015,147396");
-		assertThrows(StringIndexOutOfBoundsException.class, () -> {
-			EmployeeConverter.convertStringsToEmployees(testValidStrings);
+		testValidStrings.add("792050,Ms.,Marget,,Bowden,,marget.bowden@hotmail.com,23/10/1991,7/8/2015,147396");
+		final Employee[] actualEmployee = new Employee[1];
+		assertDoesNotThrow(() -> {
+			actualEmployee[0] = EmployeeConverter.convertStringsToEmployees(testValidStrings).get(0);
+			assertNull( actualEmployee[0].getmName());
+			assertNull( actualEmployee[0].getGender());
 		});
 	}
 	@Test
