de979a84749bbefbcc90bd166f78837f84d4bd67
Mikesjai <Michaeljai.alo@gmail.com>
2022-07-14 13:55:04 +0100

    first commit

--- src/main/java/com/sparta/AlphaTeam/core/Main.java
+++ src/main/java/com/sparta/AlphaTeam/core/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         SQLProgram sqlProgram = new SQLProgram();
         sqlProgram.start();
+        System.out.println("This is michael's message");
     }
 }
