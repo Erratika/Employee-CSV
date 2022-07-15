1aaaf8eb7fa9ed2550410e34b95eca190c19e967
M-Matson <108122866+M-Matson@users.noreply.github.com>
2022-07-15 14:23:30 +0100

    AddTask Class update

--- src/main/java/com/sparta/AlphaTeam/DataManagement/AddTask.java
+++ src/main/java/com/sparta/AlphaTeam/DataManagement/AddTask.java
@@ -1,20 +1,38 @@
 package com.sparta.AlphaTeam.DataManagement;
 
+import com.sparta.AlphaTeam.DataManagement.Database.ConnectionFactory;
+import com.sparta.AlphaTeam.DataManagement.Database.DAO;
+import com.sparta.AlphaTeam.DataManagement.Database.DatabaseInit;
+import com.sparta.AlphaTeam.DataManagement.Database.EmployeeDAO;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class AddTask implements Runnable{
     Employee[] employeeArray;
 
     public AddTask(Employee[] employeeArray) {
+
         this.employeeArray = employeeArray;
     }
 
     @Override
     public void run() {
-        for (Employee employee: employeeArray) {
-
-
+        DAO dataAccess = new EmployeeDAO();
+        for (Employee e : employeeArray){
+            dataAccess.add(e);
         }
+        /*Connection connection = ConnectionFactory.getConnection();
+        try {Statement statement = connection.createStatement();
+            for (Employee employee: employeeArray) {
+
+
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }*/
+
 
 
     }
